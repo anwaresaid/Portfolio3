2f8b85ec0bbea905900d2ff1fb20e24e96e35bdb
anwaresaid <51533569+anwaresaid@users.noreply.github.com>
2021-02-24 14:44:39 +0300

    header done

--- portfolio3/src/App.js
+++ portfolio3/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Particles, { ShapeType } from 'react-particles-js';
 
 import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Particles
+      className="partcs"
+      params={{
+        particles:{
+          number:{
+            value: 30,
+            density:{
+              enable:true,
+              value_area:900
+            }
+          },
+          shape:{
+            type:"star",
+            stroke: {
+              width:6,
+              color:"#00FFFF"
+            }
+          }
+        }
+      }}
+      />
+
       <Navbar/>
-      <h1>Portfolio3</h1>
+      <Header/>
     </div>
   );
 }
